67b0ee806064daae69e8fd5ce59ea4f5c8ba25b1
hanyahunya <java020103@gmail.com>
2025-06-16 02:29:19 +0900

    [add] パスワードをエンコードして保存する機能追加

--- src/main/java/hanyahunya/stock/users/UserServiceImpl.java
+++ src/main/java/hanyahunya/stock/users/UserServiceImpl.java
@@ -2,6 +2,7 @@ package hanyahunya.stock.users;
 
 import hanyahunya.stock.users.dto.JoinUserDto;
 import hanyahunya.stock.util.ResponseDto;
+import hanyahunya.stock.util.service.EncodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,17 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final EncodeService encodeService;
 
     @Override
     public ResponseDto<Void> join(JoinUserDto joinUserDto) {
         joinUserDto.setUserId(UUID.randomUUID().toString());
-        User savedUser = userRepository.save(joinUserDto.toEntity());
-        return null;
+        try {
+            joinUserDto.setPassword(encodeService.encode(joinUserDto.getPassword()));
+            userRepository.save(joinUserDto.toEntity());
+            return ResponseDto.success("会員登録に成功しました");
+        } catch (Exception e) {
+            return ResponseDto.fail("会員登録に失敗しました");
+        }
     }
 }
