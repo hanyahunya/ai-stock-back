daca700dc15642faee1f1e267f8b0e0492b22522
hanyahunya <java020103@gmail.com>
2025-06-17 00:14:46 +0900

    [add] ユーザー情報の取得機能を追加（仮）

--- src/main/java/hanyahunya/stock/users/UserController.java
+++ src/main/java/hanyahunya/stock/users/UserController.java
@@ -1,14 +1,12 @@
 package hanyahunya.stock.users;
 
 import hanyahunya.stock.users.dto.JoinUserDto;
+import hanyahunya.stock.users.dto.UserInfoDto;
 import hanyahunya.stock.util.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static hanyahunya.stock.util.ResponseUtil.toResponse;
 
@@ -23,4 +21,8 @@ public class UserController {
         return toResponse(userService.join(reqDto));
     }
 
+    @GetMapping("/{loginId}")
+    public ResponseEntity<ResponseDto<UserInfoDto>> getUserTest(@PathVariable String loginId) {
+
+    }
 }
