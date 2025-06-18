b5756ec3b2fdf398d2c65e0e7c425dc9ae5c9482
hanyahunya <java020103@gmail.com>
2025-06-19 02:26:38 +0900

    [add] open api(kiwoom)からデータを受ける機能追加

--- src/main/java/hanyahunya/stock/external/kiwoom/KiwoomService.java
+++ src/main/java/hanyahunya/stock/external/kiwoom/KiwoomService.java
@@ -2,19 +2,19 @@ package hanyahunya.stock.external.kiwoom;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hanyahunya.stock.external.kiwoom.dto.StockDetailDto;
+import hanyahunya.stock.external.kiwoom.dto.StockDetailListDto;
 import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 @Service
 public class KiwoomService {
@@ -26,12 +26,14 @@ public class KiwoomService {
 
     private String accessToken;
 
-    private static final String API_URL = "https://api.kiwoom.com/oauth2/token";
+    private static final String API_URL = "https://api.kiwoom.com";
+    private final HttpHeaders headers = new HttpHeaders();
     private final RestTemplate restTemplate = new RestTemplate();
 
     @PostConstruct
     public void init() {
         setToken();
+        headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
     void setToken() {
@@ -40,12 +42,9 @@ public class KiwoomService {
         body.put("appkey", appKey);
         body.put("secretkey", secretKey);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
 
-        ResponseEntity<String> response = restTemplate.postForEntity(API_URL, entity, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(API_URL + "/oauth2/token", entity, String.class);
 
         if (!response.getStatusCode().is2xxSuccessful()) {
             return;
@@ -59,4 +58,47 @@ public class KiwoomService {
             System.out.println(e.getMessage());
         }
     }
+
+    public Map<LocalDate, StockDetailDto> getDailyStockDetails(String stockCode, LocalDate date) {
+        Map<LocalDate, StockDetailDto> resultMap = new TreeMap<>();
+
+        headers.set("Authorization", accessToken);
+        headers.set("cont-yn", "Y");
+        headers.set("api-id", "ka10086");
+
+        Map<String, String> body = new HashMap<>();
+        body.put("stk_cd", stockCode);
+        body.put("qry_dt", date.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        body.put("indc_tp", "1"); // 0: 数量、1:100万ウォン 単位
+
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<StockDetailListDto> response = restTemplate.exchange(API_URL + "/api/dostk/mrkcond", HttpMethod.POST, entity, StockDetailListDto.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            for (StockDetailDto dto : response.getBody().getStockDetails()) {
+                // 絶対値処理
+                dto.setOpenPrice(Math.abs(dto.getOpenPrice()));
+                dto.setHighPrice(Math.abs(dto.getHighPrice()));
+                dto.setLowPrice(Math.abs(dto.getLowPrice()));
+                dto.setClosePrice(Math.abs(dto.getClosePrice()));
+                // 文字列の数値前処理
+                dto.setIndividual(rawParseInt(dto.getIndividualRaw()));
+                dto.setFore(rawParseInt(dto.getForeRaw()));
+                dto.setInstitution(rawParseInt(dto.getInstitutionRaw()));
+                dto.setProgram(rawParseInt(dto.getProgramRaw()));
+
+                resultMap.put(dto.getDate(), dto);
+            }
+        }
+        return resultMap;
+    }
+
+    private int rawParseInt(String input) {
+        if (input.contains("--")) {
+            String replaced = input.replace("--", "-");
+            return Integer.parseInt(replaced);
+        }
+        return Integer.parseInt(input);
+    }
+
 }
