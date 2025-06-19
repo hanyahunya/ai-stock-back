78d5030d5828ab5b58f43d6cb104d9292288465d
hanyahunya <java020103@gmail.com>
2025-06-20 03:15:37 +0900

    [add] user_stockに保存機能を追加

--- src/main/java/hanyahunya/stock/userStocks/UserStockService.java
+++ src/main/java/hanyahunya/stock/userStocks/UserStockService.java
@@ -1,4 +1,8 @@
 package hanyahunya.stock.userStocks;
 
+import hanyahunya.stock.userStocks.dto.UserStockDto;
+import hanyahunya.stock.util.ResponseDto;
+
 public interface UserStockService {
+    ResponseDto<Void> addUserStock(UserStockDto userStockDto);
 }
--- src/main/java/hanyahunya/stock/userStocks/UserStockServiceImpl.java
+++ src/main/java/hanyahunya/stock/userStocks/UserStockServiceImpl.java
@@ -1,4 +1,23 @@
 package hanyahunya.stock.userStocks;
 
+import hanyahunya.stock.userStocks.dto.UserStockDto;
+import hanyahunya.stock.userStocks.entity.UserStock;
+import hanyahunya.stock.util.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserStockServiceImpl implements UserStockService {
+    private final UserStockRepository userStockRepository;
+
+    @Override
+    public ResponseDto<Void> addUserStock(UserStockDto userStockDto) {
+        try {
+            userStockRepository.save(UserStock.of(userStockDto.getUserId(), userStockDto.getStockCode()));
+            return ResponseDto.success("保存に成功しました");
+        } catch (Exception e) {
+            return ResponseDto.fail("保存に失敗しました");
+        }
+    }
 }
