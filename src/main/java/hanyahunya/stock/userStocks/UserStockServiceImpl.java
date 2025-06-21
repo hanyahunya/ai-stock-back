4fcdc749a2bb9541ec36bb8b95b90938bad39902
hanyahunya <java020103@gmail.com>
2025-06-21 20:01:08 +0900

    [add] user_stockに取得機能を追加

--- src/main/java/hanyahunya/stock/userStocks/UserStockServiceImpl.java
+++ src/main/java/hanyahunya/stock/userStocks/UserStockServiceImpl.java
@@ -1,10 +1,14 @@
 package hanyahunya.stock.userStocks;
 
 import hanyahunya.stock.userStocks.dto.UserStockDto;
+import hanyahunya.stock.userStocks.dto.UserStockListDto;
 import hanyahunya.stock.userStocks.entity.UserStock;
 import hanyahunya.stock.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,19 @@ public class UserStockServiceImpl implements UserStockService {
             return ResponseDto.success("保存に成功しました");
         } catch (Exception e) {
             return ResponseDto.fail("保存に失敗しました");
+        }
+    }
+
+    @Override
+    public ResponseDto<UserStockListDto> getUserStock(String userId) {
+        List<UserStockDto> resList = new ArrayList<>();
+        try {
+            userStockRepository.findUserStockByUser_UserId(userId).forEach(userStock -> {
+                resList.add(UserStockDto.toDto(userStock));
+            });
+            return ResponseDto.success("読み込みに成功しました", UserStockListDto.set(resList));
+        } catch (Exception e) {
+            return ResponseDto.fail("読み込みに失敗しました");
         }
     }
 }
